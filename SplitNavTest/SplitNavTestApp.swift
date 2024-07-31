eeb9357a1733c4772d05c939f925a16480477b5c
erik <eloehfelm@mac.com>
2024-07-31 09:25:59 -0400

    Initial Commit

---
+++ SplitNavTest/SplitNavTestApp.swift
@@ -0,0 +1,33 @@
+//
+//  SplitNavTestApp.swift
+//  SplitNavTest
+//
+//  Created by Erik Loehfelm on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitNavTestApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ SplitNavTestTests/SplitNavTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplitNavTestTests.swift
+//  SplitNavTestTests
+//
+//  Created by Erik Loehfelm on 7/31/24.
+//
+
+import Testing
+@testable import SplitNavTest
+
+struct SplitNavTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
